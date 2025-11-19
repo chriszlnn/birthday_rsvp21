3e44d19b3e72cd7828e6d0c86be13fbbf3cd7109
Chrislyn <chrislynjules@gmail.com>
2025-11-19 20:55:53 +0800

    Fixed Date 2

--- backend/routes/calendarRoutes.js
+++ backend/routes/calendarRoutes.js
@@ -7,9 +7,10 @@ const router = express.Router();
 // iOS Calendar (.ics file)
 router.get('/ios', (req, res) => {
   const event = {
-    start: [2025, 11, 28, 20, 0], // e.g., 8:00 PM MYT (20:00)
-    end:   [2025, 11, 29, 0, 0],   // e.g., 12:00 AM MYT (00:00 next day)
-    timezone: 'Asia/Kuala_Lumpur', // 12 AM MYT -> 4 PM UTC
+    startInputType: "local",
+    endInputType: "local",
+    start: [2025, 11, 28, 20, 0],  // 8PM MYT
+    end:   [2025, 11, 29, 0, 0],  // 12 AM MYT -> 4 PM UTC
     title: "Chrislyn's 21st Birthday - Disco Fever!",
     description: "DRESS CODE: DISCO DRIP OR RETRO FIT✨",
     location: 'ARTE CHERAS',
